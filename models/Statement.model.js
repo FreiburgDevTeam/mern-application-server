6960c6120ff2f27f42c72b23666c38fd2e4b63aa
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-05-19 15:08:54 +0200

    create statement model

---
+++ models/Statement.model.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require("mongoose");
+
+const statementSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: [True, "Title is required"],
+        },
+        description: String,
+        amout: {
+            type: Number,
+            min: 1,
+            max: 1000000,
+            required: [True, "Amount is required"],
+        },
+        type: {
+            type: String,
+            enum: ["income", "expense"],
+            required: [True, "Type is required"],
+        },
+        regularity: {
+            type: String,
+            enum: ["once", "monthly"],
+            required: [True, "Regularity is required"],
+            default: "once"
+        },
+        startDate: {
+            type: Date,
+            default: Date.now,
+            required: [True, "Start date is required"],
+        },
+        category: {
+            type: String,
+            enum: ["Job", "Travel", "Investments", "Hobby", "Sport", "Entertaiment", "Food", "Health", "Transport", "Others"],
+            default: "Others"
+        },
+
+    },
+    {
+        // this second object adds extra properties: `createdAt` and `updatedAt`
+        timestamps: true,
+    }
+);
+
+const Statement = model("Statement", statementSchema);
+
+module.exports = User;
