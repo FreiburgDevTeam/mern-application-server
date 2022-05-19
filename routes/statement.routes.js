27f79de2e7de219a738587c5f23d0b2aa1ec71d6
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-05-19 17:56:45 +0200

    add route to get list and details of statements

--- routes/statement.routes.js
+++ routes/statement.routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { default: mongoose } = require("mongoose");
 const Statement = require("../models/Statement.model");
 
-//Create new Statement
+// Create new Statement
 router.post('/', (req, res, next) => {
     const { title, amount, description, type, regularity, startDate, category } = req.body;
     const newStatement = { title, amount, description, type, regularity, startDate, category }
@@ -19,5 +19,41 @@ router.post('/', (req, res, next) => {
             });
         })
 })
+
+// Get list of statements
+router.get("/", (req, res, next) => {
+    Statement.find()
+        .then(response => {
+            res.json(response)
+        })
+        .catch(err => {
+            console.log("error getting list of statements", err);
+            res.status(500).json({
+                message: "error getting list of statements",
+                error: err
+            });
+        })
+});
+
+//  Get details of a specific statement by id
+router.get('/:statementId', (req, res, next) => {
+    
+    const { statementId } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(statementId)) {
+        res.status(400).json({ message: 'Specified id is not valid' });
+        return;
+    }
+
+    Statement.findById(statementId)
+        .then(statement => res.json(statement))
+        .catch(err => {
+            console.log("error getting details of a project", err);
+            res.status(500).json({
+                message: "error getting details of a project",
+                error: err
+            });
+        })
+});
 
 module.exports = router;
